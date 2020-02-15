c20678cae362448967097a1a765b23b4d68a2c8e
Arisor <1171723054@qq.com>
2020-02-16 00:31:58 +0800

    路由prefix

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = require('koa-router')(),
     login = require('../controllers/login'),
     register = require('../controllers/register'),
     message = require('../controllers/message');
-
+    router.prefix(`/api`)
 router.post('/login', login)
     .post('/register', register)
     .post('/message', verify, message) // 获取首页列表信息
